162f6c7a375b910e0f804ce59bb7ffa85738cf52
Walesca <117771096+wadolelo@users.noreply.github.com>
2024-11-04 22:05:05 -0300

    mudei o url

--- feedbackSobreMim.js
+++ feedbackSobreMim.js
@@ -6,7 +6,7 @@ async function redesSociaisFavoritasMinhaEscola() {
         const dadosLocais = JSON.parse(dadosLocaisString)
         processarDados(dadosLocais)
     } else {
-        const url = 'https://script.googleusercontent.com/a/macros/escola.pr.gov.br/echo?user_content_key=_68a1L1KPaxAec2o620o4iuL6LTuxs32YoiHa2iMEP1BfDjR1izOl0FAPtBNMFzEXFYQXoTrlEuWckmyMpAHYtEtPk-aROeQOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKBGCNaBo701naU8cM07sy4y4YFj_89rZFU4i0YbRrPFa0nlMLg8OKcLrR3OPiCTrsc5OYz34oA0fW6kSzS8G5RcdCpe9nzFfnV3zG1Xep3RG1oXT2NdSlX22F2V4riqsgcYZpTak_o22Q&lib=MbcU3kD1mfSwJjef8Z8aS7UyDORvBQL-x'
+        const url = 'https://script.google.com/macros/s/AKfycbwqHNm8KvjvI1Ca0DBuaMaHq72zLd3XSjAxcyh5Y9nPXO8HAJvIqFumCaqg4fb7EMQU4Q/exec'
         const res = await fetch(url)
         const dados = await res.json()
         localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
